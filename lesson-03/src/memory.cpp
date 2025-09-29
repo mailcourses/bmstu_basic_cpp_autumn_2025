fc1d043d15d8d37b063fc42788244dc0dd56ea6f
a.kukhtichev <a.kukhtichev@mail.ru>
2025-09-30 02:41:12 +0300

    Добавил материалы с лекций

--- lesson-03/src/memory.cpp
+++ lesson-03/src/memory.cpp
@@ -14,6 +14,14 @@ int foo(int num)
     return staticj;
 }
 
+/*
+ * class string {
+ *   size_t size_;
+ *   size_t capacity_;
+ *   char* data_;
+ * }
+ */
+
 int main()
 {
     for (size_t i = 0; i < 5; ++i)
@@ -22,7 +30,20 @@ int main()
         std::cout << "j=" << res << ", i=" << statici << std::endl;
     }
 
-    std::string* str = (std::string*)malloc(sizeof(std::string));
+    //std::string* str = (std::string*)malloc(sizeof(std::string));
+    std::string* str = new std::string;
     str->push_back('a');
+    std::cout << "str: " << str << std::endl;
+    int* a = new int{100500};
+    std::cout << "address of a is " << a << std::endl;
+
+    const int * const pointer = new int{500100};
+    //pointer = a;
+    //*pointer = 5;
+
+    int& magic = globali;
+    std::cout << globali << " === " << magic << std::endl;
+    magic = -10;
+    std::cout << globali << " === " << magic << std::endl;
 }
 
